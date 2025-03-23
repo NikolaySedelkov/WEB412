23e26aa31a41e543704390845a82f7a4063e008a
Your Name <you@example.com>
2025-03-23 11:19:00 +0300

    useState введение

--- src/ButtonPageUp.jsx
+++
@@ -1,9 +0,0 @@
-import styles from  './ButtonPageUp.module.css';
-
-export function ButtonPageUp() {
-    return (
-        <div className={styles["grid-window-logo"]}>
-            ^
-        </div>
-    )
-}
--- src/components/Battery/index.js
+++
@@ -1,1 +0,0 @@
-export { Battery } from './Battery'
